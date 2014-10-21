45f80f28000452a504a653d026f28ccd71e3899d
Robert Stoll <rstoll@tutteli.ch>
2014-10-21 20:44:14 +0200

    added dummy test to fix build

---
+++ test/ch/tsphp/tinsphp/common/test/DummyTest.java
@@ -0,0 +1,19 @@
+/*
+ * This file is part of the TinsPHP project published under the Apache License 2.0
+ * For the full copyright and license information, please have a look at LICENSE in the
+ * root folder or visit the project's website http://tsphp.ch/wiki/display/TINS/License
+ */
+
+package ch.tsphp.tinsphp.common.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest
+{
+    @Test
+    public void dummyTestInOrderThatBuildPasses(){
+        assertTrue(true);
+    }
+}
