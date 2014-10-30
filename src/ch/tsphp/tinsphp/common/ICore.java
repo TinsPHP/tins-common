4ef3d855a3e1cdd396346917dacec0029ae08937
Robert Stoll <rstoll@tutteli.ch>
2014-10-30 08:22:06 +0100

    - TINS-165 move type system to core - updated build.properties (was data from tsphp-common)

---
+++ src/ch/tsphp/tinsphp/common/ICore.java
@@ -0,0 +1,82 @@
+/*
+ * This file is part of the TinsPHP project published under the Apache License 2.0
+ * For the full copyright and license information, please have a look at LICENSE in the
+ * root folder or visit the project's website http://tsphp.ch/wiki/display/TINS/License
+ */
+
+package ch.tsphp.tinsphp.common;
+
+/**
+ * Represents the core of TinsPHP which defines the built-in primitive types, functions,
+ * classes and interfaces as well as available operator overloading and implicit/explicit casts.
+ */
+public interface ICore
+{
+//    Map<Integer, List<IMethodSymbol>> getUnaryOperators();
+//
+//    Map<Integer, List<IMethodSymbol>> getBinaryOperators();
+//
+//    Map<ITypeSymbol, Map<ITypeSymbol, ICastingMethod>> getImplicitCasting();
+//
+//    Map<ITypeSymbol, Map<ITypeSymbol, ICastingMethod>> getExplicitCastings();
+//
+//    IVoidTypeSymbol getVoidTypeSymbol();
+//
+//    INullTypeSymbol getNullTypeSymbol();
+//
+//    IScalarTypeSymbol getBoolTypeSymbol();
+//
+//    IScalarTypeSymbol getBoolFalseableTypeSymbol();
+//
+//    IScalarTypeSymbol getBoolNullableTypeSymbol();
+//
+//    IScalarTypeSymbol getBoolFalseableAndNullableTypeSymbol();
+//
+//    IScalarTypeSymbol getIntTypeSymbol();
+//
+//    IScalarTypeSymbol getIntFalseableTypeSymbol();
+//
+//    IScalarTypeSymbol getIntNullableTypeSymbol();
+//
+//    IScalarTypeSymbol getIntFalseableAndNullableTypeSymbol();
+//
+//    IScalarTypeSymbol getFloatTypeSymbol();
+//
+//    IScalarTypeSymbol getFloatFalseableTypeSymbol();
+//
+//    IScalarTypeSymbol getFloatNullableTypeSymbol();
+//
+//    IScalarTypeSymbol getFloatFalseableAndNullableTypeSymbol();
+//
+//    IScalarTypeSymbol getStringTypeSymbol();
+//
+//    IScalarTypeSymbol getStringFalseableTypeSymbol();
+//
+//    IScalarTypeSymbol getStringNullableTypeSymbol();
+//
+//    IScalarTypeSymbol getStringFalseableAndNullableTypeSymbol();
+//
+//    IArrayTypeSymbol getArrayTypeSymbol();
+//
+//    IArrayTypeSymbol getArrayFalseableTypeSymbol();
+//
+//    IPseudoTypeSymbol getResourceTypeSymbol();
+//
+//    IPseudoTypeSymbol getResourceFalseableTypeSymbol();
+//
+//    IPseudoTypeSymbol getMixedTypeSymbol();
+//
+//    IClassTypeSymbol getExceptionTypeSymbol();
+//
+//    ICastingMethod getStandardCastingMethod(ITypeSymbol formalParameterType);
+//
+//    void addExplicitCastFromTo(ITypeSymbol actualParameterType, ITypeSymbol formalParameterType);
+//
+//    void addExplicitCastFromTo(ITypeSymbol actualParameterType, ITypeSymbol formalParameterType,
+//            ICastingMethod castingMethod);
+//
+//    void addImplicitCastFromTo(ITypeSymbol actualParameterType, ITypeSymbol formalParameterType);
+//
+//    void addImplicitCastFromTo(ITypeSymbol actualParameterType, ITypeSymbol formalParameterType,
+//            ICastingMethod castingMethod);
+}
