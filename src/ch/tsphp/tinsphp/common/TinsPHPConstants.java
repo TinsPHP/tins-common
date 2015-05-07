1405e1f1efaedb1d1634aa3a6053d7736a9b33e8
Robert Stoll <rstoll@tutteli.ch>
2015-05-07 15:03:15 +0200

    TINS-450 rename and move TypeVariableNames
    - moved TypeVariableNames from tins-symbols to this component and renamed it to TinsPHPConstants

---
+++ src/ch/tsphp/tinsphp/common/TinsPHPConstants.java
@@ -0,0 +1,15 @@
+/*
+ * This file is part of the TinsPHP project published under the Apache License 2.0
+ * For the full copyright and license information, please have a look at LICENSE in the
+ * root folder or visit the project's website http://tsphp.ch/wiki/display/TINS/License
+ */
+
+package ch.tsphp.tinsphp.common;
+
+public final class TinsPHPConstants
+{
+    public static final String RETURN_VARIABLE_NAME = "rtn";
+
+    private TinsPHPConstants() {
+    }
+}
