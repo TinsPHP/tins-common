8ef711ff215bd47af2841159ccdefb107c5fd315
Robert Stoll <rstoll@tutteli.ch>
2015-05-07 15:58:02 +0200

    TINS-453 move BoundException to tins-common
    - moved the from tins-symbols to this component

---
+++ src/ch/tsphp/tinsphp/common/inference/constraints/IntersectionBoundException.java
@@ -0,0 +1,18 @@
+/*
+ * This file is part of the TinsPHP project published under the Apache License 2.0
+ * For the full copyright and license information, please have a look at LICENSE in the
+ * root folder or visit the project's website http://tsphp.ch/wiki/display/TINS/License
+ */
+
+package ch.tsphp.tinsphp.common.inference.constraints;
+
+import ch.tsphp.common.symbols.ITypeSymbol;
+import ch.tsphp.tinsphp.common.symbols.IIntersectionTypeSymbol;
+
+public class IntersectionBoundException extends UpperBoundException
+{
+    public IntersectionBoundException(
+            String message, IIntersectionTypeSymbol theUpperTypeBound, ITypeSymbol theNewLowerType) {
+        super(message, theUpperTypeBound, theNewLowerType);
+    }
+}
---
+++ src/ch/tsphp/tinsphp/common/inference/constraints/UpperBoundException.java
@@ -0,0 +1,31 @@
+/*
+ * This file is part of the TinsPHP project published under the Apache License 2.0
+ * For the full copyright and license information, please have a look at LICENSE in the
+ * root folder or visit the project's website http://tsphp.ch/wiki/display/TINS/License
+ */
+
+package ch.tsphp.tinsphp.common.inference.constraints;
+
+import ch.tsphp.common.symbols.ITypeSymbol;
+import ch.tsphp.tinsphp.common.symbols.IIntersectionTypeSymbol;
+
+public class UpperBoundException extends BoundException
+{
+    private IIntersectionTypeSymbol upperTypeBound;
+    private ITypeSymbol newLowerType;
+
+    public UpperBoundException(
+            String message, IIntersectionTypeSymbol theUpperTypeBound, ITypeSymbol theNewLowerType) {
+        super(message);
+        upperTypeBound = theUpperTypeBound;
+        newLowerType = theNewLowerType;
+    }
+
+    public IIntersectionTypeSymbol getUpperTypeBound() {
+        return upperTypeBound;
+    }
+
+    public ITypeSymbol getNewLowerType() {
+        return newLowerType;
+    }
+}
