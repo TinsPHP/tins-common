7ad25c933582cd927421cb0699d4ee78d18d9f6b
Robert Stoll <rstoll@tutteli.ch>
2015-05-10 17:20:04 +0200

    TINS-423 QA 0.3.1: Pair documented

--- src/ch/tsphp/tinsphp/common/utils/Pair.java
+++ src/ch/tsphp/tinsphp/common/utils/Pair.java
@@ -6,12 +6,17 @@
 
 package ch.tsphp.tinsphp.common.utils;
 
+/**
+ * A simple immutable pair with fields first and second.
+ *
+ * @param <TFirst>  The type of the field first
+ * @param <TSecond> The type of the field second
+ */
 @SuppressWarnings("checkstyle:visibilitymodifier")
 public final class Pair<TFirst, TSecond>
 {
     public final TFirst first;
     public final TSecond second;
-
 
     public Pair(TFirst firstItem, TSecond secondItem) {
         first = firstItem;
